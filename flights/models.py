b3b98cfd5dcf05bc532a49b43d3235fcb00ced21
abuaminuu <i.abuaminu@gmail.com>
2026-09-24 12:49:48 +0100

    fix: add django-environ

--- flights/models.py
+++ flights/models.py
@@ -11,9 +11,6 @@ class Airport(models.Model):
         return f"{self.city} ({self.code})"
 
 class Flight(models.Model):
-    #references Airport class, and if an aiport is deleted from Airport
-    #delete all flights that have that airport
-    #related name: to obtain all flights with said origin if you have airport name
     origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="departures")
     destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="arrivals")
     duration = models.IntegerField()
@@ -27,7 +24,7 @@ class Flight(models.Model):
 class Passenger(models.Model):
     first = models.CharField(max_length=64)
     last = models.CharField(max_length=64)
-    #many to many relationships, flight can have many passengers and a passenger can have many flights
+    # many to many relationships, flight can have many passengers and a passenger can have many flights
     flights = models.ManyToManyField(Flight, blank=True, related_name="passengers")
 
     def __str__(self):
